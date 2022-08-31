9d46f022737d54b031f55d2db5efd90133133974
Ankit <ankit.bhojani@mindinventory.com>
2022-08-31 18:59:52 +0530

    added changes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const express = require('express');
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-
 const app = express();
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
